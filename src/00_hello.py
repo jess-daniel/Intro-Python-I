7837024bb675994f7a8fad723cb62cf407cac39f
Jess Daniel <jessdaniel474@gmail.com>
2020-07-08 17:01:04 -0400

    fixed 11_args file

--- src/00_hello.py
+++ src/00_hello.py
@@ -3,20 +3,20 @@ print("Hello, world!")
 
 # Func for guided
 # Solution 1
-def num_str(a, b):
-    if type(a) == str and type(b) == str:
-        return int(a) + int(b)
-    elif type(a) == int and type(b) == int:
-        return str(a) + str(b)
-    else:
-        return None
+# def num_str(a, b):
+#     if type(a) == str and type(b) == str:
+#         return int(a) + int(b)
+#     elif type(a) == int and type(b) == int:
+#         return str(a) + str(b)
+#     else:
+#         return None
 
-# Solution 2
-def num_str2(a, b):
-    if type(a) == type(b):
-        if type(a) is str:
-            return int(a) + int(b)
-        return str(a) + str(b)
+# # Solution 2
+# def num_str2(a, b):
+#     if type(a) == type(b):
+#         if type(a) is str:
+#             return int(a) + int(b)
+#         return str(a) + str(b)
 
-print(num_str("1",2))
-print(num_str2("1", 2))
+# print(num_str("1",2))
+# print(num_str2("1", 2))
